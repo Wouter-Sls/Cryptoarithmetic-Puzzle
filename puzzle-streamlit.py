c19fefcabadbf34d8f191f1c53058ba7df24d335
Wouter Selis <113997013+Wouter-Sls@users.noreply.github.com>
2023-09-28 20:20:21 +0200

    Add files via upload

--- puzzle-streamlit.py
+++ puzzle-streamlit.py
@@ -1,5 +1,4 @@
 from simpleai.search import CspProblem, backtrack
-from streamlit_js_eval import streamlit_js_eval
 import streamlit as st
 
 ########################################################################## USER INPUT #######################################
@@ -134,8 +133,3 @@ if isFilledIn:
     #Display the letters with their values on the Streamlite app
     for key, value in output.items():
         st.write(key, ' ==> ', str(value))
-
-    
-
-    if st.button("Refresh"):
-        streamlit_js_eval(js_expressions="parent.window.location.reload()")
